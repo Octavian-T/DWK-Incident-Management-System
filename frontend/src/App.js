cf4e1d4a438bcfde8e2d6ec4353c3dffe973cb82
billywba <billy.wba8@gmail.com>
2020-05-27 17:51:14 +0100

    added css for login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,24 @@ import Login from './components/Login';
 import ViewIncidents from './components/ViewIncidents';
 import Error from './components/Error';
 
+import './components/css/Main.css';
+
 
 function App() {
   return (
     <>
-      <h1>DWK PLC Incident Management System</h1>
-
-      <Navbar />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/login" component={Login} />
-        <Route path="/incidents" component={ViewIncidents} />
-        <Route component={Error} />
-      </Switch>
+      <div className="main-header">
+        <h1>DWK PLC</h1>
+      </div>
+      
+      <div className="content-container">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/login" component={Login} />
+          <Route path="/incidents" component={ViewIncidents} />
+          <Route component={Error} />
+        </Switch>
+      </div>
     </>
   );
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import './css/Login.css';
+
 function Login() {
   return (
     <>
-      <h2>Login</h2>
+      <h2 className="subheading">Incident Reporting</h2>
+
+      <div className="login-container">
+        <form>
+          <input type="text" placeholder="Username"></input>
+          <br></br>
+          <input type="password" placeholder="Password"></input>
+          <br></br>
+          <button>Log in</button>
+        </form>
+      </div>
     </>
   );
 }
